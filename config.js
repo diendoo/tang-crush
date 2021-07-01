01c0359756cd4153c095dadd38b2c517b1c38142
diendoo <86765024+diendoo@users.noreply.github.com>
2021-07-01 13:56:44 +0700

    Create config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'tối này 10h bắn pubg  với a nhé hihi.',
+    messDesc: 'tối này 7h đi khách sạn với a nhé hihi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' /messages/t/100028090521079
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
